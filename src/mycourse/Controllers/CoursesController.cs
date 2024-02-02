7bd481a4c30bb4a3ff8376fd4002b89c00d527a9
Luca Bertolino <lucasart07@gmail.com>
2024-02-02 13:28:27 +0100

    lastCommit

--- src/mycourse/Controllers/CoursesController.cs
+++ src/mycourse/Controllers/CoursesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using mycourse.Models.Services.Application;
 
 namespace mycourse.Controllers
 {
@@ -10,7 +11,9 @@ namespace mycourse.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var courseService = new CourseService();
+            List<CourseViewModel> courses = courseService.GetServices();
+            return View(courses);
         }
 
         public IActionResult Detail(string id)
---
+++ src/mycourse/Models/Services/Application/CourseService.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mycourse.Models.Services.Application
+{
+    public class CourseService
+    {
+        public List<CourseViewModel> GetServices()
+        {
+            throw new NotImplementedException();
+        }
+        
+    }
+}
